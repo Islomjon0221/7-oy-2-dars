268ca3f18152d49af693fdbd583e73421932cef4
Islomjon0221 <islomjon20110221@gmail.com>
2024-04-07 21:47:18 +0500

    Vazifa Tayyor

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,22 @@ function App() {
   const name = useRef()
   const age = useRef()
   function validate() {
-    if(!name.current.value.trim()) {
+    if(!name.current.value) {
       setUserErr(true)
       alert("Name is null")
       return false
+    }
+    else {
+      setUserErr(false)
+    }
+
+    if(name.current.value.trim().length < 3) {
+      setUserErr(true)
+      alert("Name must have more than 3 words")
+      return false
+    }
+    else {
+      setUserErr(false)
     }
     
     if(!age.current.value) {
@@ -22,17 +34,17 @@ function App() {
       alert("Age is null")
       return false
     }
+    else {
+      setAgeErr(false)
+    }
 
-    if(age.current.value.length > 100 && age.current.value.length < 4) {
+    if(age.current.value > 100 || age.current.value < 4) {
       setAgeErr(true)
       alert("Should be more than 3 years of age than 100")
       return false
     }
-
-    if(name.current.value.trim().length < 3) {
-      setUserErr(true)
-      alert("Name must have more than 3 words")
-      return false
+    else {
+      setAgeErr(false)
     }
     
     return true
@@ -54,10 +66,10 @@ function App() {
       <h3 className='text-xl font-bold mb-[70px] mt-[30px] text-center text-[40px]'>Alifreza<span className='text-xl font-bold text-[40px] text-[#079211]'>.</span></h3>
       <form className='ml-[40px] mr-[40px] flex flex-col'>
         <h3 className='text-bold text-[25px]'>Name <span className='text-[#079211]'>*</span></h3>
-        <input ref={name} type="text" className={`rounded bg-black mt-[10px] border-b-4 border-[#079211] focus:outline-none placeholder:text-white`} placeholder='Name...' />
+        <input ref={name} type="text" className={`rounded bg-black ${userErr == true ? "border-red-700" : "border-[#079211]"} mt-[10px] border-b-4 border-[#079211] focus:outline-none placeholder:text-white`} placeholder='Name...' />
 
         <h3 className='text-bold mt-[40px] text-[25px]'>Age <span className='text-[#079211]'>*</span></h3>
-        <input ref={age} type="number" className='rounded mb-[40px] bg-black mt-[10px] border-b-4 border-[#079211] focus:outline-none placeholder:text-white' placeholder='Age...' />
+        <input ref={age} type="number" className={`rounded mb-[40px] ${ageErr == true ? "border-red-700" : "border-[#079211]"} bg-black mt-[10px] border-b-4 border-[#079211] focus:outline-none placeholder:text-white`} placeholder='Age...' />
 
         <button onClick={handleClick} className='font-bold text-sm outline px-[88px] rounded mb-[40px] py-[5px] bg-[#079211] outline-[#079211] hover:bg-black duration-300'>Save</button>
       </form>
